17cefc6e32d815c857fc9a4e0c2c5074aa94d799
Higor Santos <higoraugusto120@gmail.com>
2021-10-13 20:22:11 -0300

    subindo projeto urna

---
+++ etapas.js
@@ -0,0 +1,77 @@
+let etapas = [
+    {
+      titulo: 'VEREADOR',
+      numeros: 5,
+      candidatos: [
+        {
+          numero: '38111',
+          nome: 'Fulano',
+          partido: 'ABC',
+          fotos:[
+            {url:'cebolinha.jpg', legenda: 'Vereador', small:false}
+            ]
+        },
+        {
+          numero: '77222',
+          nome: 'Beltrano da Silva',
+          partido: 'DEFG',
+          fotos:[
+            {url: 'magali.jpg', legenda: 'Vereador'}
+            
+            ]
+          
+          
+        },
+        
+        
+        
+        
+        
+        
+        
+        ]
+    },
+    
+    {
+      titulo:'PREFEITO',
+      numeros:2,
+      candidatos: [
+        {
+          numero:'99',
+          nome: 'Ciclano',
+          partido: 'ABC',
+          vice: 'Cic',
+          fotos:[
+            {url:'monica.png', legenda: 'Prefeito'},
+            {url: 'magali.jpg', legenda: 'Vice-Prefeito', small: true}
+            
+            ]
+        },
+        {
+          numero:'84',
+          nome: 'Zulando',
+          partido: 'QWERTY',
+          vice: 'Zul',
+          fotos:[
+            
+            {url:'cascão.png', legenda: 'Prefeito'},
+            {url:'cebolinha.jpg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        
+        
+        
+        
+        ]
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ];
